141f0bbdaf1b6272925ad84ee2ae373de68abaa7
erikrios <erikriosetiawan49@gmail.com>
2019-12-30 19:54:12 +0700

    Use the data class for the nickname in the TextView

--- app/src/main/java/com/erikriosetiawan/aboutme/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/aboutme/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.myName = myName
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         binding.doneButton.setOnClickListener {
             addNickName(it)
         }
